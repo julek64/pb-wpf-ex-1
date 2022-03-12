833e81036a17a7779ea769de3fd9dce7a9d19f38
Juliusz Zagdański <juliusz644@gmail.com>
2022-03-12 11:03:51 +0100

    Add coupon machine model

---
+++ pb-wpf-ex-1/Models/CouponMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pb_wpf_ex_1.Models
+{
+    public class CouponMachine
+    {
+        private readonly ICollection<string> coupons = new List<string>();
+
+        public void Add(string coupon)
+        {
+            coupons.Add(coupon);
+        }
+
+        public string TakeRandom()
+        {
+            int randomIndex = new Random().Next(0, coupons.Count);
+            string randomCoupon = coupons.ElementAt(randomIndex);
+
+            return coupons.Remove(randomCoupon) ? randomCoupon : throw new Exception("Removing coupon was not successful");
+        }
+
+        public bool IsEmpty() => coupons.Count == 0;
+
+        public IReadOnlyCollection<string> PeekCoupons()
+        {
+            return coupons.ToList().AsReadOnly();
+        }
+    }
+}

6ce5594e742f52bf1b778d4bf9bcd3ca6d97dda7
Juliusz Zagdański <juliusz644@gmail.com>
2022-03-12 11:40:57 +0100

    Add coupon machine view

--- pb-wpf-ex-1/MainWindow.xaml.cs
+++ pb-wpf-ex-1/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using pb_wpf_ex_1.Models;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace pb_wpf_ex_1
 {
@@ -20,9 +10,53 @@ namespace pb_wpf_ex_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly CouponMachine couponMachine;
+
+        public TextBox AddCouponTextBox { get; set; }
+        public Label TakeCouponLabel { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            couponMachine = new();
+            machineStateLabel.Content = $"Current machine state: {string.Join(", ", couponMachine.PeekCoupons())}";
+        }
+
+        private void OnAddButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (addCouponTextBox.Text == "") return;
+            
+            try
+            {
+                couponMachine.Add(addCouponTextBox.Text);
+                messageLabel.Content = $"Coupon {addCouponTextBox.Text} was added to the machine";
+            }
+            catch (Exception)
+            {
+                messageLabel.Content = "Coupon could not be added to the machine";
+            }
+            machineStateLabel.Content = $"Current machine state: {string.Join(", ", couponMachine.PeekCoupons())}";
+        }
+
+        private void OnTakeButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (couponMachine.IsEmpty())
+            {
+                messageLabel.Content = "Machine is empty";
+                return;
+            }
+
+            try
+            {
+                string coupon = couponMachine.TakeRandom();
+                messageLabel.Content = $"Following coupon was taken out of the machine randomly: {coupon}";
+            }
+            catch (Exception)
+            {
+                messageLabel.Content = "No coupon could be taken from the machine";
+            }
+
+            machineStateLabel.Content = $"Current machine state: {string.Join(", ", couponMachine.PeekCoupons())}";
         }
     }
 }
